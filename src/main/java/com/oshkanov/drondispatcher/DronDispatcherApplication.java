17a39c1d42bc6b400f3ee50aa1e585dd6717b5d0
Vladislav Oshkanov <ovs@live.ru>
2022-11-19 14:33:14 +0700

    Add project frame and database

---
+++ src/main/java/com/oshkanov/drondispatcher/DronDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.oshkanov.drondispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronDispatcherApplication.class, args);
+	}
+
+}
